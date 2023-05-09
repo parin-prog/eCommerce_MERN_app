0f44d3178a5e526c624141cb133bfe0131ad2d06
parin-prog <parinvaghani001@gmail.com>
2023-05-09 16:42:36 +0530

    Update user by verifying token and authorization

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
-app = express();
+app = express()
 const mongoose = require("mongoose")
-const dotenv  = require("dotenv");
+const dotenv  = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,31 @@
+const User = require('../models/User');
+const { verifyTokenAndAuthorization } = require('./middleware/verifyToken');
+
 const router = require('express').Router();
 
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SECR
+        ).toString();
+    }
 
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body
+            },
+            { new: true }
+        );
+        const {password, ...others} = updatedUser._doc;
+        res.status(200).json({...others})
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
