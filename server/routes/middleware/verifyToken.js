916c6a24f4e6b44da0bed07d86307c55ae7f0f71
parin-prog <parinvaghani001@gmail.com>
2023-05-10 15:05:45 +0530

    fetching multiple user data with url query

--- server/routes/middleware/verifyToken.js
+++ server/routes/middleware/verifyToken.js
@@ -25,4 +25,14 @@ const verifyTokenAndAuthorization = (req,res,next) => {
     })
 }
 
-module.exports = { verifyToken,verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req,res,next) => {
+    verifyToken(req,res, ()=>{
+        if(req.user.isAdmin){
+            next();
+        } else{
+            res.status(403).json("You are not allowed to do that!")
+        }
+    })
+}
+
+module.exports = { verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,9 @@
 const User = require('../models/User');
-const { verifyTokenAndAuthorization } = require('./middleware/verifyToken');
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./middleware/verifyToken');
 
 const router = require('express').Router();
 
-// UPDATE
+// User Update
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
@@ -27,5 +27,30 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+// User Delete
+router.delete("/find/:id", verifyTokenAndAuthorization , async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted.")
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+// Get all user
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+    const query = req.query.new;   /* URL query */
+    try {
+        const users =  query
+                        ? await User.find().sort({id:-1}).limit(5)
+                        : await User.find().limit(55);
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 
 module.exports = router
